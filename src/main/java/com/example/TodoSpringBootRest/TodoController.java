1a62a6cd8d8903c978363106bae83329b25ff7c6
Ayush Kumar Yadav <ayushyadav8056@gmail.com>
2025-05-15 00:52:41 +0530

    Added basic controller structure

---
+++ src/main/java/com/example/TodoSpringBootRest/TodoController.java
@@ -0,0 +1,19 @@
+package com.example.TodoSpringBootRest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+//@Controller
+//@ResponseBody
+public class TodoController {
+    private static List<Todo> todos;
+
+    public TodoController() {
+        todos = new ArrayList<>();
+        todos.add(new Todo(1, false, "Todo 1", 1));
+        todos.add(new Todo(2, true, "Todo 2", 1));
+    }
+}
