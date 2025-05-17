18c4b6df7a9def814eca172c36f5d4716b182a72
Ayush Kumar Yadav <ayushyadav8056@gmail.com>
2025-05-17 13:26:44 +0530

    Handled status code in the response using ResposndeEntity

--- src/main/java/com/example/TodoSpringBootRest/TodoController.java
+++ src/main/java/com/example/TodoSpringBootRest/TodoController.java
@@ -1,9 +1,8 @@
 package com.example.TodoSpringBootRest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,13 +19,20 @@ public class TodoController {
         todoList.add(new Todo(2, true, "Todo 2", 1));
     }
     @GetMapping("/todos")
-    public List<Todo> getTodos() {
-        return todoList;
+    public ResponseEntity<List<Todo>> getTodos() {
+        return ResponseEntity.ok(todoList);
     }
+  // 201
     @PostMapping("/todos")
-    public Todo createTodo(@RequestBody Todo newTodo){
+
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo newTodo){
+        /**
+         * @ResponseStatus(HttpStatus.CREATED) // 201 if created
+         * qe can use above annotation to get status code
+         */
         todoList.add(newTodo);
-        return newTodo;
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(newTodo);
     }
 
 
