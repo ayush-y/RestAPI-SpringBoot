3ec7b98c531b7ef8ca68f360e2d346bce6b4f534
Ayush Kumar Yadav <ayushyadav8056@gmail.com>
2025-05-17 13:07:33 +0530

    Added Todo Creation

--- src/main/java/com/example/TodoSpringBootRest/TodoController.java
+++ src/main/java/com/example/TodoSpringBootRest/TodoController.java
@@ -1,6 +1,8 @@
 package com.example.TodoSpringBootRest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -21,4 +23,11 @@ public class TodoController {
     public List<Todo> getTodos() {
         return todoList;
     }
-}
+    @PostMapping("/todos")
+    public Todo createTodo(@RequestBody Todo newTodo){
+        todoList.add(newTodo);
+        return newTodo;
+    }
+
+
+    }
