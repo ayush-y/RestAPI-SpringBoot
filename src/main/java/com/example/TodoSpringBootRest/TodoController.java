7e2a515f244798a61f670bb8216dd313bf2be280
Ayush Kumar Yadav <ayushyadav8056@gmail.com>
2025-05-17 16:39:06 +0530

    Added Delete mapping and PachMapping API

--- src/main/java/com/example/TodoSpringBootRest/TodoController.java
+++ src/main/java/com/example/TodoSpringBootRest/TodoController.java
@@ -1,5 +1,7 @@
 package com.example.TodoSpringBootRest;
 
+import com.sun.net.httpserver.HttpsServer;
+import jakarta.websocket.server.PathParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,11 +22,20 @@ public class TodoController {
     }
     @GetMapping("/todos")
     public ResponseEntity<List<Todo>> getTodos() {
-        return ResponseEntity.ok(todoList);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(todoList);
     }
-  // 201
+    @GetMapping("/todos/{todoId}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable Long todoId) {
+            for(Todo todo : todoList){
+                if(todo.getId() == todoId){
+                    return ResponseEntity.status(HttpStatus.OK)
+                            .body(todo);
+                }
+            }
+        return ResponseEntity.notFound().build();
+    }
     @PostMapping("/todos")
-
     public ResponseEntity<Todo> createTodo(@RequestBody Todo newTodo){
         /**
          * @ResponseStatus(HttpStatus.CREATED) // 201 if created
@@ -34,6 +45,28 @@ public class TodoController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(newTodo);
     }
+    @DeleteMapping("/todos/{todoId}")
+    public ResponseEntity<Todo> deleteTodoById(@PathVariable Long todoId){
+        for(Todo todo : todoList){
+            if(todo.getId() == todoId) {
+                todoList.remove(todo);
+                return ResponseEntity.status(HttpStatus.OK)
+                        .body(todo);
+            }
 
-
+        }
+        return ResponseEntity.badRequest().build();
     }
+    @PatchMapping("/todos/{todoId}")
+    public ResponseEntity<Todo> updateTodoById(@PathVariable Long todoId, @RequestBody Todo updatedTodo){
+        for(Todo todo : todoList){
+            if(todo.getId() == todoId) {
+                todo.setCompleted(updatedTodo.isCompleted());
+                todo.setTitle(updatedTodo.getTitle());
+                return ResponseEntity.status(HttpStatus.OK)
+                        .body(todo);
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
